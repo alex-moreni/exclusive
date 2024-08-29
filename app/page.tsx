abb10eb5ab14e4b6509ca494b8bfcef92cda77ad
AlexMoreni <alex.moreni47@gmail.com>
2024-08-29 14:21:40 -0300

    chore: shadcn ui setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <main>
       <h1>Hello</h1>
+
+      <Button>Button</Button>
     </main>
   )
 }
